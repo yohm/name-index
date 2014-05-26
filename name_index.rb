5e8f7f4a02428380e39db69824c30a2947553484
Yohsuke Murase <yohsuke.murase@gmail.com>
2014-05-26 11:50:04 +0900

    first implementation

--- name_index.rb
+++ name_index.rb
@@ -1,5 +1,36 @@
 class NameIndex
+
+  KATAKANA_CHARS = %w(
+    アイウエオ
+    カキクケコガギグゲゴ
+    サシスセソザジズゼゾ
+    タチツテトダヂヅデド
+    ナニヌネノ
+    ハヒフヘホバビブベボパピプペポ
+    マミムメモ
+    ヤユヨ
+    ワヲ
+    ン
+  )
+
+  private
+  def self.get_index_char(name)
+    found_line = KATAKANA_CHARS.find do |kana_line|
+      kana_line.include?(name[0])
+    end
+    found_line[0]
+  end
+
+  public
   def self.create_index(names)
-    []
+    index = Hash.new { Array.new }
+    names.each do |name|
+      key = get_index_char(name)
+      index[key] += [name]
+    end
+    index = index.sort_by do |key,val|
+      KATAKANA_CHARS.map {|s| s[0]}.index(key)
+    end
+    index.map {|key,val| [key, val.sort] }
   end
 end
